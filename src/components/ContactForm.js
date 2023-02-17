2f3517e0dd4c2cf1bc70af5cb47af22e0cf24958
Peter Huynh <peterhyh@yahoo.com>
2023-02-17 00:45:18 -0600

    Form Update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,38 @@
-import { Formik, Form, Field} from 'formik';
+import {Formik} from 'formik';
+
+const initialValues = {
+    email: '',
+    message: ''
+};
 
 
 const ContactForm = () => {
     return(
-        <Formik>
-            
+        <Formik
+            initialValues = {initialValues}
+            validate = {validateForm}
+            onSubmit = {handleSubmit}
+        >
+            {
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <input 
+                            value = {values.email}
+                            name = 'email'
+                            onChange = {handleChange}
+                            placeholder = 'Your email'
+                        />
+                    </div>
+                    <div>
+                        <input
+                            value = {values.message}
+                            name = 'message'
+                            onChange = {handleChange}
+                            placeholder = 'Enter message...'
+                        />
+                    </div>
+                </form>
+            }
         </Formik>
     )
 };
