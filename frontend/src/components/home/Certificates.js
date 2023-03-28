f87d122d1d64c5a60db6531199439819175ff20c
Peter Huynh <peterhyh@yahoo.com>
2023-03-27 20:38:22 -0500

    tested deployment

--- frontend/src/components/home/Certificates.js
+++ frontend/src/components/home/Certificates.js
@@ -1,103 +1,103 @@
-import { Container, Row, Col } from 'reactstrap';
-import FS from '../../app/assets/img/honorFullStack.png';
-import BE from '../../app/assets/img/honorBackend.png';
-import python from '../../app/assets/img/python.png';
-import webDev from '../../app/assets/img/webDevFundamentals.png';
-import sql from '../../app/assets/img/sql.png';
-import devOps from '../../app/assets/img/devops.png';
-import BS from '../../app/assets/img/bootstrap.png';
-import react from '../../app/assets/img/react.png';
-import RN from '../../app/assets/img/reactNative.png';
-import mongo from '../../app/assets/img/mongodb.png';
+// import { Container, Row, Col } from 'reactstrap';
+// import FS from '../../app/assets/img/honorFullStack.png';
+// import BE from '../../app/assets/img/backend.png';
+// import python from '../../app/assets/img/python.png';
+// import webDev from '../../app/assets/img/webDevFundamentals.png';
+// import sql from '../../app/assets/img/sql.png';
+// import devOps from '../../app/assets/img/devops.png';
+// import BS from '../../app/assets/img/bootstrap.png';
+// import react from '../../app/assets/img/react.png';
+// import RN from '../../app/assets/img/reactNative.png';
+// import mongo from '../../app/assets/img/mongodb.png';
 
-const Certificates = () => {
-    return (
-        <Container>
-            <Row>
-                <Col className='certificates-section d-flex justify-content-center mt-3'>
-                    <h1>Certificates</h1>
-                </Col>
-            </Row>
-
-
-            <Row className='d-flex justify-content-center'>
-                <Col className='d-flex justify-content-center mt-3' md='4'>
-                    <img className='honor-certificate' src={FS} alt='Full Stack Honor Certificate' />
-                </Col>
-                <Col className='d-flex justify-content-center mt-3' md='4'>
-                    <img className='honor-certificate' src={BE} alt='Backend Honor Certificate' />
-                </Col>
-            </Row>
-            <Row className='d-flex justify-content-center'>
-                <Col className='d-flex justify-content-center p-3' md='4'>
-                    <p>Full Stack Honor</p>
-                </Col>
-                <Col className='d-flex justify-content-center p-3' md='4'>
-                    <p>Backend Honor</p>
-                </Col>
-            </Row>
+// const Certificates = () => {
+//     return (
+//         <Container>
+//             <Row>
+//                 <Col className='certificates-section d-flex justify-content-center mt-3'>
+//                     <h1>Certificates</h1>
+//                 </Col>
+//             </Row>
 
 
-            <Row className='d-flex justify-content-center'>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={webDev} alt='Web Development Fundamentals Certificate' />
-                </Col>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={python} alt='Data Structure and Algorithms with Python Certificate' />
-                </Col>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={sql} alt='SQL and Data Modeling with Python Certificate' />
-                </Col>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={devOps} alt='Modern Software Engineering with DevOps Certificate' />
-                </Col>
-            </Row>
-            <Row className='d-flex justify-content-center'>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>Web Development</p>
-                </Col>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>Python</p>
-                </Col>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>SQL</p>
-                </Col>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>DevOps</p>
-                </Col>
-            </Row>
+//             <Row className='d-flex justify-content-center'>
+//                 <Col className='d-flex justify-content-center mt-3' md='4'>
+//                     <img className='honor-certificate' src={FS} alt='Full Stack Honor Certificate' />
+//                 </Col>
+//                 <Col className='d-flex justify-content-center mt-3' md='4'>
+//                     <img className='honor-certificate' src={BE} alt='Backend Honor Certificate' />
+//                 </Col>
+//             </Row>
+//             <Row className='d-flex justify-content-center'>
+//                 <Col className='d-flex justify-content-center p-3' md='4'>
+//                     <p>Full Stack Honor</p>
+//                 </Col>
+//                 <Col className='d-flex justify-content-center p-3' md='4'>
+//                     <p>Backend Honor</p>
+//                 </Col>
+//             </Row>
 
 
-            <Row className='d-flex justify-content-center'>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={BS} alt='Front-End Web Development: Bootstrap' />
-                </Col>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={react} alt='Front-End Web Development: React' />
-                </Col>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={RN} alt='Front End Web and Mobile Development Bootcamp' />
-                </Col>
-                <Col className='d-flex justify-content-center mt-3' md='3'>
-                    <img className='fs-certificate' src={mongo} alt='Back-End Development: NodeJS, Express, MongoDB' />
-                </Col>
-            </Row>
-            <Row className='d-flex justify-content-center'>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>Bootstrap</p>
-                </Col>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>React</p>
-                </Col>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>React Native</p>
-                </Col>
-                <Col className='d-flex justify-content-center p-3' md='3'>
-                    <p>NodeJS, Express, MongoDB</p>
-                </Col>
-            </Row>
-        </Container>
-    )
-};
+//             <Row className='d-flex justify-content-center'>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={webDev} alt='Web Development Fundamentals Certificate' />
+//                 </Col>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={python} alt='Data Structure and Algorithms with Python Certificate' />
+//                 </Col>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={sql} alt='SQL and Data Modeling with Python Certificate' />
+//                 </Col>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={devOps} alt='Modern Software Engineering with DevOps Certificate' />
+//                 </Col>
+//             </Row>
+//             <Row className='d-flex justify-content-center'>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>Web Development</p>
+//                 </Col>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>Python</p>
+//                 </Col>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>SQL</p>
+//                 </Col>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>DevOps</p>
+//                 </Col>
+//             </Row>
 
-export default Certificates;
+
+//             <Row className='d-flex justify-content-center'>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={BS} alt='Front-End Web Development: Bootstrap' />
+//                 </Col>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={react} alt='Front-End Web Development: React' />
+//                 </Col>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={RN} alt='Front End Web and Mobile Development Bootcamp' />
+//                 </Col>
+//                 <Col className='d-flex justify-content-center mt-3' md='3'>
+//                     <img className='fs-certificate' src={mongo} alt='Back-End Development: NodeJS, Express, MongoDB' />
+//                 </Col>
+//             </Row>
+//             <Row className='d-flex justify-content-center'>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>Bootstrap</p>
+//                 </Col>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>React</p>
+//                 </Col>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>React Native</p>
+//                 </Col>
+//                 <Col className='d-flex justify-content-center p-3' md='3'>
+//                     <p>NodeJS, Express, MongoDB</p>
+//                 </Col>
+//             </Row>
+//         </Container>
+//     )
+// };
+
+// export default Certificates;
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
         <div>
             <HomeBanner />
             <ProfileContent />
-            <Certificates />
+            {/* <Certificates /> */}
         </div>
     )
 };
