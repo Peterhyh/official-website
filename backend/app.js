4a2880e1e64a3fc6bf2d6a0f1d764c27bf0e7341
Peter Huynh <peterhyh@yahoo.com>
2023-04-03 22:14:16 -0500

    Backend attempt: 2

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,9 @@ const logger = require('morgan');
 const passport = require('passport');
 const config = require('./config.js');
 
-
-
 const indexRouter = require('./routers/index');
 const usersRouter = require('./routers/users');
 const contactRouter = require('./routers/contactRouter');
-const bmiRouter = require('./routers/bmiRouter');
-
-
 
 const mongoose = require('mongoose');
 const url = config.mongoUrl;
@@ -25,15 +20,6 @@ connect.then(() => console.log('Connected correctly to server!'), err => console
 
 const app = express();
 
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  } else {
-    console.log(`Redirecting to https://${req.hostname}:${app.get('secPort')}${req.url}`);
-    res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
-  }
-});
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -42,25 +28,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
-
-
 app.use(passport.initialize());  //check to see if there is an existing session for the client
 // app.use(passport.session());  if so, session data will be loaded into the req as "req.user"
-
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
 app.use('/contact', contactRouter);
-app.use('/bmi', bmiRouter);
-
-
-
-
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -77,7 +52,6 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
 
 
 module.exports = app;
