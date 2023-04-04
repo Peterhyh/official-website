77fd045a7df8a4720a90f75161654a8425b8210c
Peter Huynh <peterhyh@yahoo.com>
2023-04-04 12:52:06 -0500

    docker ignore updated for backend

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,8 @@
 import Certificates from '../components/home/Certificates';
 import HomeBanner from '../components/home/HomeBanner';
 import ProfileContent from '../components/home/ProfileContent';
-import Projects from '../components/home/Projects';
-import ContactForm from '../components/home/ContactForm';
+// import Projects from '../components/home/Projects';
+// import ContactForm from '../components/home/ContactForm';
 
 
 const HomePage = () => {
