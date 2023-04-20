0a51ab2ba7a10d4a4278b36494030331687cc19b
Peter Huynh <peterhyh@yahoo.com>
2023-04-19 20:03:46 -0500

    frontend profilecontent update

--- frontend/src/components/home/ProfileContent.js
+++ frontend/src/components/home/ProfileContent.js
@@ -1,62 +1,38 @@
-import { Container, Col, Row } from 'reactstrap';
+
 
 
 const ProfileContent = () => {
 
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+            } else {
+                entry.target.classList.remove('show');
+            }
+        })
+    })
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+
+    hiddenElements.forEach(element => observer.observe(element));
+
     return (
-        <div className='bio-container'>
-            <div className='profile-bio-container' >
-                <div >
-                    <span >
-                        Hi there! My name is Peter Huynh and I graduated the backend and full stack programs
-                        at Nucamp with honors. I have a great passion for coding and ready to put my skills to
-                        the test in a professional setting. I also enjoy photography, automotives, 3D printing,
-                        and building gaming PC's. I love figuring out how something works and being able to
-                        replicate/build it myself and eventually make it better than the original. My favorite part
-                        about the process is discovering an original idea of my own along the way.
-                    </span>
-                </div>
-            </div>
-            <div className='profile-title-container' >
-                <div className='intro-title' >
-                    <h1 >About Me</h1>
-                </div>
-            </div >
-        </div>
-
-
+        <>
+            <section class='hidden'>
+                <h1 class='bio-title'>About Me</h1>
+                <p class='bio-description'>Hi there! My name is Peter Huynh and I graduated the backend and full stack programs
+                    at Nucamp with honors. I have a great passion for coding and ready to put my skills to
+                    the test in a professional setting. I also enjoy photography, automotives, 3D printing,
+                    and building gaming PC's. I love figuring out how something works and being able to
+                    replicate/build it myself and eventually make it better than the original. My favorite part
+                    about the process is discovering an original idea of my own along the way.
+                </p>
+            </section>
+        </>
     )
 
 }
 
-
-
 export default ProfileContent;
-
-
-
-
-
-// <Card className='about_me mt-5' style={{ backgroundColor: '#1E1E1EC4' }}>
-        //     <CardBody className='p-5'>
-        //         <Row className='d-flex align-items-center'>
-        //             <Col className='d-flex justify-content-center m-3' md='7'>
-        //                 <Card className='profile_message border-5 border-dark p-4' style={{ backgroundColor: '#61625DC4' }}>
-        //                     <CardTitle>
-        //                         About Me
-        //                     </CardTitle>
-        //                     <CardText>
-        //                         My name is Peter Huynh and I learned how to create this website after
-        //                         completing the software engineering program at Nucamp. During my time at Nucamp,
-        //                         I experienced Python, SQL, DevOps, Bootstrap, React and React Native. I'm currently
-        //                         looking for a job position as a fullstack engineer. Please feel free to email me
-        //                         using the contact page.
-        //                     </CardText>
-        //                 </Card>
-        //             </Col>
-        //             <Col className='d-flex justify-content-center m-3' sm md='4'>
-        //                 <img className='rounded' src={me} alt='Photo of Peter Huynh' />
-        //             </Col>
-        //         </Row>
-        //     </CardBody>
-        // </Card >
