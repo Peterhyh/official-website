80fe2e9cadb844aabce2dde7b135d47afd0f38bd
Peter Huynh <peterhyh@yahoo.com>
2023-04-27 15:59:46 -0500

    HomePage update

--- frontend/src/components/home/ProfileContent.js
+++ frontend/src/components/home/ProfileContent.js
@@ -1,36 +1,40 @@
-
+import { useState, useEffect } from 'react';
 
 
 const ProfileContent = () => {
 
+    const [slideIn, setSlideIn] = useState(false);
+
+    useEffect(() => {
+
+        aboutme.forEach(element => observer.observe(element));
+    },);
+
     const observer = new IntersectionObserver(entries => {
         entries.forEach(entry => {
-            console.log(entry);
             if (entry.isIntersecting) {
-                entry.target.classList.add('show');
+                setSlideIn(true);
             } else {
-                entry.target.classList.remove('show');
+                setSlideIn(false);
             }
         })
     })
 
-    const hiddenElements = document.querySelectorAll('.hidden');
+    const aboutme = document.querySelectorAll('.aboutme');
 
-    hiddenElements.forEach(element => observer.observe(element));
+
 
     return (
-        <>
-            <section class='hidden'>
-                <h1 class='bio-title'>About Me</h1>
-                <p class='bio-description'>Hi there! My name is Peter Huynh and I graduated the backend and full stack programs
-                    at Nucamp with honors. I have a great passion for coding and ready to put my skills to
-                    the test in a professional setting. I also enjoy photography, automotives, 3D printing,
-                    and building gaming PC's. I love figuring out how something works and being able to
-                    replicate/build it myself and eventually make it better than the original. My favorite part
-                    about the process is discovering an original idea of my own along the way.
-                </p>
-            </section>
-        </>
+        <div className='bio-container'>
+            <h1 className={slideIn ? 'aboutme-show' : 'aboutme'}>About Me</h1>
+            <p className='bio'>Hi there! My name is Peter Huynh and I graduated the backend and full stack programs
+                at Nucamp with honors. I have a great passion for coding and ready to put my skills to
+                the test in a professional setting. I also enjoy photography, automotives, 3D printing,
+                and building gaming PC's. I love figuring out how something works and being able to
+                replicate/build it myself and eventually make it better than the original. My favorite part
+                about the process is discovering an original idea of my own along the way.
+            </p>
+        </div>
     )
 
 }
