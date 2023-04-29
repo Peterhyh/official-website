12f4ee6e5dd36104a7b7eac78535257d733bbfa9
Peter Huynh <peterhyh@yahoo.com>
2023-04-29 15:03:08 -0500

    Card color update

--- frontend/src/components/home/projects/ProjectDemoCards.js
+++ frontend/src/components/home/projects/ProjectDemoCards.js
@@ -14,19 +14,21 @@ const ProjectDemoCards = () => {
                     <div className='image-container'>
                         <img src={SweetlyDental} alt='' />
                     </div>
-                    <div className='card-title'>
-                        <h3>SweetlyDental</h3>
-                    </div>
                     <div className='card-body'>
-                        <p>{'Fullstack website designed for a dentistry built with MERN'}</p>
-                    </div>
-                    <div className='card-buttons'>
-                        <a href='http://sweetlydental.com' target='_blank' rel='noopener'>
-                            <img className='projects-button' src={ComputerSVG} alt='Computer icon' />
-                        </a>
-                        <a color='primary' outline href='https://github.com/Peterhyh/dental-website' target='_blank' rel='noopener'>
-                            <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
-                        </a>
+                        <div className='card-title'>
+                            <h3>SweetlyDental</h3>
+                        </div>
+                        <div>
+                            <p>{'Fullstack website designed for a dentistry built with MERN'}</p>
+                        </div>
+                        <div className='card-buttons'>
+                            <a href='http://sweetlydental.com' target='_blank' rel='noopener'>
+                                <img className='projects-button' src={ComputerSVG} alt='Computer icon' />
+                            </a>
+                            <a color='primary' outline href='https://github.com/Peterhyh/dental-website' target='_blank' rel='noopener'>
+                                <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
+                            </a>
+                        </div>
                     </div>
                 </div>
 
@@ -34,19 +36,21 @@ const ProjectDemoCards = () => {
                     <div className='image-container'>
                         <img src={Blackjack} alt='' />
                     </div>
-                    <div className='card-title'>
-                        <h3>Blackjack CLI Game</h3>
-                    </div>
                     <div className='card-body'>
-                        <p>{'Blackjack CLI game was created using Python'}</p>
-                    </div>
-                    <div className='card-buttons'>
-                        <a href='https://www.youtube.com/watch?v=_VLc779betI&ab_channel=PeterHuynh' target='_blank' rel='noopener'>
-                            <img className='projects-button' src={YouTubeSVG} alt='Computer icon' />
-                        </a>
-                        <a color='primary' outline href='https://github.com/Peterhyh/BlackJack' target='_blank' rel='noopener'>
-                            <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
-                        </a>
+                        <div className='card-title'>
+                            <h3>Blackjack CLI Game</h3>
+                        </div>
+                        <div>
+                            <p>{'Blackjack CLI game was created using Python'}</p>
+                        </div>
+                        <div className='card-buttons'>
+                            <a href='https://www.youtube.com/watch?v=_VLc779betI&ab_channel=PeterHuynh' target='_blank' rel='noopener'>
+                                <img className='projects-button' src={YouTubeSVG} alt='Computer icon' />
+                            </a>
+                            <a color='primary' outline href='https://github.com/Peterhyh/BlackJack' target='_blank' rel='noopener'>
+                                <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
+                            </a>
+                        </div>
                     </div>
                 </div>
 
@@ -54,19 +58,21 @@ const ProjectDemoCards = () => {
                     <div className='image-container'>
                         <img src={Pharmacy} alt='' />
                     </div>
-                    <div className='card-title'>
-                        <h3>Pharmacy Database</h3>
-                    </div>
                     <div className='card-body'>
-                        <p>{'Blackjack CLI game was created using Python'}</p>
-                    </div>
-                    <div className='card-buttons'>
-                        <a href='http://herbapharmacy.com' target='_blank' rel='noopener'>
-                            <img className='projects-button' src={ComputerSVG} alt='Computer icon' />
-                        </a>
-                        <a color='primary' outline href='https://github.com/Peterhyh/prescripton-database-2.0' target='_blank' rel='noopener'>
-                            <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
-                        </a>
+                        <div className='card-title'>
+                            <h3>Pharmacy Database</h3>
+                        </div>
+                        <div>
+                            <p>{'Blackjack CLI game was created using Python'}</p>
+                        </div>
+                        <div className='card-buttons'>
+                            <a href='http://herbapharmacy.com' target='_blank' rel='noopener'>
+                                <img className='projects-button' src={ComputerSVG} alt='Computer icon' />
+                            </a>
+                            <a color='primary' outline href='https://github.com/Peterhyh/prescripton-database-2.0' target='_blank' rel='noopener'>
+                                <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
