88dbd330dc92461b2935d0e4536003317cb5e817
Peter Huynh <peterhyh@yahoo.com>
2023-04-29 22:06:01 -0500

    Projects css update

--- frontend/src/components/home/projects/ProjectDemoCards.js
+++ frontend/src/components/home/projects/ProjectDemoCards.js
@@ -4,6 +4,7 @@ import GitHubSVG from '../../../app/assets/img/github.svg';
 import Blackjack from '../../../app/assets/img/blackjack.png';
 import YouTubeSVG from '../../../app/assets/img/youtube.svg';
 import Pharmacy from '../../../app/assets/img/pharmacy.png';
+import './Projects.css';
 
 const ProjectDemoCards = () => {
     return (
