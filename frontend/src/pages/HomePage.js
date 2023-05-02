59b1fe4a4cd3447b83b9f8aad6021b9afb807263
Peter Huynh <peterhyh@yahoo.com>
2023-05-01 21:08:29 -0500

    deployment 2

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -2,13 +2,11 @@ import Certificates from '../components/home/certificate/Certificates';
 import ProfileContent from '../components/home/bio/ProfileContent';
 import ProjectDemoCards from '../components/home/projects/ProjectDemoCards';
 import Skills from '../components/home/skills/Skills';
-import HomeBanner from '../components/home/banner/HomeBanner';
 
 
 const HomePage = () => {
     return (
         <div>
-            {/* <HomeBanner /> */}
             <ProfileContent />
             <Skills />
             <ProjectDemoCards />
