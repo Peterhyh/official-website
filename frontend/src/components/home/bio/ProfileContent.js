8a91b16ce4f4b9f0070b450fb3565ffa04a0d3e1
Peter Huynh <peterhyh@yahoo.com>
2023-05-12 16:36:09 -0500

    Design update

--- frontend/src/components/home/bio/ProfileContent.js
+++ frontend/src/components/home/bio/ProfileContent.js
@@ -2,26 +2,25 @@
 // import Typewriter from "typewriter-effect";
 import './Bio.css';
 import Signiture from '../../../app/assets/img/bsigniture.png';
+import ProfilePic from '../../../app/assets/img/originalProfilePic.jpg';
 
 const ProfileContent = () => {
 
     return (
         <div className='bio-container'>
-            <div className='bio-row'>
-                <h1 className='bio-title'>About Me</h1>
-            </div>
-            <div className='bio-row'>
-                <p className='bio-description'>
-                    Hi there! My name is Peter Huynh and I graduated the backend and full stack programs
-                    at Nucamp with honors. I have a great passion for coding and ready to put my skills to
-                    the test in a professional setting. I also enjoy photography, automotives, 3D printing,
-                    and building gaming PC's. I love figuring out how something works and being able to
-                    replicate/build it myself and eventually make it better than the original. My favorite part
-                    about the process is discovering an original idea of my own along the way.
-                </p>
-            </div>
-            <div className='bio-row'>
-                <img src={Signiture} alt='' />
+            <h1 className='bio-title'>About Me</h1>
+            <p className='bio-description'>
+                Hi there! My name is Peter Huynh and I graduated the backend and full stack programs
+                at Nucamp with honors. I have a great passion for coding and ready to put my skills to
+                the test in a professional setting. I also enjoy photography, automotives, 3D printing,
+                and building gaming PC's. I love figuring out how something works and being able to
+                replicate/build it myself and eventually make it better than the original. My favorite part
+                about the process is discovering an original idea of my own along the way.
+            </p>
+            <div className='profilePicContainer'>
+                <div className='profile-picture'>
+                    <img src={ProfilePic} alt='' />
+                </div>
             </div>
         </div>
     )
