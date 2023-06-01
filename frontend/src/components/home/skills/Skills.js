9d931be2c9fc6eadf5151a0ddd0bde8eeae85aed
Peter Huynh <peterhyh@yahoo.com>
2023-05-31 21:56:01 -0500

    Skills icon added

--- frontend/src/components/home/skills/Skills.js
+++ frontend/src/components/home/skills/Skills.js
@@ -2,6 +2,14 @@ import './Skills.css';
 import { Parallax } from 'react-scroll-parallax';
 import HTML from '../../../app/assets/img/html.svg';
 import CSS from '../../../app/assets/img/css.svg';
+import REACT from '../../../app/assets/img/react.svg';
+import JS from '../../../app/assets/img/javascript.svg';
+import DOCKER from '../../../app/assets/img/docker.svg';
+import EXPRESS from '../../../app/assets/img/express.svg';
+import GITHUB from '../../../app/assets/img/github.svg';
+import MONGO from '../../../app/assets/img/mongodb.svg';
+import GIT from '../../../app/assets/img/git.svg';
+import PYTHON from '../../../app/assets/img/python.svg';
 
 const Skills = () => {
 
@@ -10,13 +18,18 @@ const Skills = () => {
             <Parallax translateX={['1000px', '-300px']}>
                 <p className='skills-title'>SKILLS</p>
             </Parallax>
-            <Parallax
-                translateX={[0, 100]}
-                easing={'easeOutQuad'}
-            >
+            <div className='skillIconContainer'>
                 <img className='skillIcon' src={HTML} alt='HTML' />
                 <img className='skillIcon' src={CSS} alt='CSS' />
-            </Parallax>
+                <img className='skillIcon' src={REACT} alt='REACT' />
+                <img className='skillIcon' src={JS} alt='JavaScript' />
+                <img className='skillIcon' src={DOCKER} alt='DOCKER' />
+                <img className='skillIcon' src={EXPRESS} alt='EXPRESS' />
+                <img className='skillIcon' src={GITHUB} alt='GITHUB' />
+                <img className='skillIcon' src={MONGO} alt='MONGO' />
+                <img className='skillIcon' src={GIT} alt='GIT' />
+                <img className='skillIcon' src={PYTHON} alt='PYTHON' />
+            </div>
         </div>
     )
 };
