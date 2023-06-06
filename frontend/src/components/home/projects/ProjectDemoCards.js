80c29f574f5474ee09a6e7951fd935c03b6dd60f
Peter Huynh <peterhyh@yahoo.com>
2023-06-05 20:11:11 -0500

    Updating skills section

--- frontend/src/components/home/projects/ProjectDemoCards.js
+++ frontend/src/components/home/projects/ProjectDemoCards.js
@@ -8,80 +8,48 @@ import './Projects.css';
 import { Parallax } from 'react-scroll-parallax';
 
 const ProjectDemoCards = () => {
+
+    const projects = [
+        {
+            id: 0,
+            title: 'Herb-A-Pharmacy Demo',
+            img: Pharmacy,
+            description: 'Fullstack MERN pharmacy database application allows you to store prescription information for patients. This application allows you to register as an authorized user to input patient information.'
+        },
+        {
+            id: 1,
+            title: 'Sweetly Dental Demo',
+            img: SweetlyDental,
+            description: 'Fullstack MERN pharmacy database application allows you to store prescription information for patients. This application allows you to register as an authorized user to input patient information.'
+        },
+    ];
+
     return (
         <div className='projects-container'>
             <Parallax translateX={['-1000px', '300px']}>
                 <p className='project-title'>PROJECTS</p>
             </Parallax>
 
-            <div className='project-row'>
-                <div className='project-card'>
-                    <div className='image-container'>
-                        <img src={SweetlyDental} alt='' />
-                    </div>
-                    <div className='card-body'>
-                        <div className='card-title'>
-                            <h3>SweetlyDental</h3>
+            {projects.map((project, i) => (
+                <div className='projectCard'>
+                    <div className='projectContent'>
+                        <div className='projectDescriptionContainer'>
+                            <div className='projectTitle'>
+                                <h1>{project.title}</h1>
+                            </div>
+                            <div className='projectDescription'>
+                                <p>{project.description}</p>
+                            </div>
                         </div>
-                        <div>
-                            <p>{'Fullstack website designed for a dentistry built with MERN'}</p>
-                        </div>
-                        <div className='card-buttons'>
-                            <a href='http://sweetlydental.com' target='_blank' rel='noopener'>
-                                <img className='projects-button' src={ComputerSVG} alt='Computer icon' />
-                            </a>
-                            <a color='primary' outline href='https://github.com/Peterhyh/dental-website' target='_blank' rel='noopener'>
-                                <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
-                            </a>
+                        <div className='projectImage'>
+                            <img src={project.img} alt='' />
                         </div>
                     </div>
+
                 </div>
 
-                <div className='project-card'>
-                    <div className='image-container'>
-                        <img src={Blackjack} alt='' />
-                    </div>
-                    <div className='card-body'>
-                        <div className='card-title'>
-                            <h3>Blackjack CLI Game</h3>
-                        </div>
-                        <div>
-                            <p>{'Blackjack CLI game was created using Python'}</p>
-                        </div>
-                        <div className='card-buttons'>
-                            <a href='https://www.youtube.com/watch?v=_VLc779betI&ab_channel=PeterHuynh' target='_blank' rel='noopener'>
-                                <img className='projects-button' src={YouTubeSVG} alt='Computer icon' />
-                            </a>
-                            <a href='https://github.com/Peterhyh/BlackJack' target='_blank' rel='noopener'>
-                                <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
-                            </a>
-                        </div>
-                    </div>
-                </div>
+            ))}
 
-
-                <div className='project-card'>
-                    <div className='image-container'>
-                        <img src={Pharmacy} alt='' />
-                    </div>
-                    <div className='card-body prescription-description'>
-                        <div className='card-title'>
-                            <h3>Herb-A-Pharmacy</h3>
-                        </div>
-                        <div>
-                            <p>{'Fullstack MERN pharmacy database application allows you to store prescription information for patients. This application allows you to register as an authorized user to input patient information.'}</p>
-                        </div>
-                        <div className='card-buttons'>
-                            <a href='http://herbapharmacy.com' target='_blank' rel='noopener'>
-                                <img className='projects-button' src={ComputerSVG} alt='Computer icon' />
-                            </a>
-                            <a href='https://github.com/Peterhyh/prescripton-database-2.0' target='_blank' rel='noopener'>
-                                <img className='projects-button' src={GitHubSVG} alt='GitHub icon' />
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
 
         </div>
     )
@@ -89,10 +57,3 @@ const ProjectDemoCards = () => {
 
 export default ProjectDemoCards;
 
-{/* <div className='projects-button-container d-flex justify-content-center align-items-center justify-content-evenly'>
-                            
-                        </div>
-
-<div className='projects-button-container d-flex justify-content-center align-items-center justify-content-evenly'>
-                            
-                        </div> */}
